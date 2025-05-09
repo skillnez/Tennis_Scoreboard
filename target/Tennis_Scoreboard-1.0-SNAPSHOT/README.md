93ff2792498258ac170c29728e959a8f03ec3099
skillnez <skillnez05@gmail.com>
2025-05-09 21:11:40 +0300

    chore: project minor refactor

--- target/Tennis_Scoreboard-1.0-SNAPSHOT/README.md
+++
@@ -1,4 +0,0 @@
-Вёрстка для проекта "Табло теннисного матча" из моего Java роадмапа
-
-- ТЗ проекта - https://zhukovsd.github.io/java-backend-learning-course/projects/tennis-scoreboard/
-- Роадмап - https://zhukovsd.github.io/java-backend-learning-course/
--- target/Tennis_Scoreboard-1.0-SNAPSHOT/js/app.js
+++
@@ -1,9 +0,0 @@
-// JavaScript for toggling the dropdown menu
-document.addEventListener("DOMContentLoaded", function () {
-    const navToggle = document.querySelector(".nav-toggle");
-    const navLinks = document.querySelector(".nav-links");
-
-    navToggle.addEventListener("click", function () {
-        navLinks.classList.toggle("active");
-    });
-});
